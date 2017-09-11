ca34651f56e98fb59b8d1dfb19236894ba02a4f3
Yuriy Zubaha <yuriy.zubaha@gmail.com1>
2017-09-11 18:33:59 +0300

    Create filter by gearbox and car

--- src/app/instructors/instructor-list.component.ts
+++ src/app/instructors/instructor-list.component.ts
@@ -12,7 +12,29 @@ export class InstructorListComponent implements OnInit {
   imgWidth: number = 50;
   imgMargin: number = 2;
   showImg: boolean = false;
-  gearboxFilter: string = 'механика';
+  // gearboxFilter: string = 'механика';
+
+  _carFilter: string = '';
+  get carFilter(): string {
+    return this._carFilter;
+  }
+  set carFilter(value: string) {
+    this._carFilter = value;
+    this.filteredInstructors = (this.gearboxFilter || this.carFilter) ?
+                                this.performFilter(this.gearboxFilter, this.carFilter) : this.instructors;
+  }
+
+  _gearboxFilter: string;
+  get gearboxFilter(): string {
+    return this._gearboxFilter;
+  }
+  set gearboxFilter(value: string) {
+    this._gearboxFilter = value;
+    this.filteredInstructors = (this.gearboxFilter || this.carFilter) ?
+                                this.performFilter(this.gearboxFilter, this.carFilter) : this.instructors;
+  }
+
+  filteredInstructors: IInstructor[];
   instructors: IInstructor[] = [
     {
     "instructorId": 1,
@@ -22,7 +44,7 @@ export class InstructorListComponent implements OnInit {
     "cityarea": "Центр",
     "price90min": 190,
     "starRating": 4.5,
-    "photoUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png",
+    "photoUrl": "http://res.cloudinary.com/yuzub/image/upload/v1505128983/panda_vmckn8.jpg",
     "photoCarUrl": "",
     "email": "",
     "phone": ""
@@ -32,10 +54,10 @@ export class InstructorListComponent implements OnInit {
     "instructorName": "Соловьев Вадим Александрович",
     "car": "ZAZ Vida",
     "gearbox":"механика",
-    "cityarea": "Центр",
+    "cityarea": "Центр Тополь",
     "price90min": 200,
     "starRating": 3.5,
-    "photoUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png",
+    "photoUrl": "http://res.cloudinary.com/yuzub/image/upload/v1505128983/panda_vmckn8.jpg",
     "photoCarUrl": "",
     "email": "",
     "phone": ""
@@ -45,15 +67,33 @@ export class InstructorListComponent implements OnInit {
     "instructorName": "Разумный Юрий Викторович",
     "car": "Chevrolet Lachetti",
     "gearbox":"механика",
-    "cityarea": "Центр",
+    "cityarea": "Центр Тополь",
     "price90min": 200,
     "starRating": 4.0,
-    "photoUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png",
+    "photoUrl": "http://res.cloudinary.com/yuzub/image/upload/v1505128983/panda_vmckn8.jpg",
     "photoCarUrl": "",
     "email": "",
     "phone": ""
-    }
+    },
+    {
+      "instructorId": 3,
+      "instructorName": "Владимиров Владислав Владимирович",
+      "car": "Nissan Note",
+      "gearbox":"автомат",
+      "cityarea": "Центр",
+      "price90min": 300,
+      "starRating": 4.0,
+      "photoUrl": "http://res.cloudinary.com/yuzub/image/upload/v1505128983/panda_vmckn8.jpg",
+      "photoCarUrl": "",
+      "email": "",
+      "phone": ""
+      }
   ];
+
+  constructor() {
+    this.filteredInstructors = this.instructors;
+    this.gearboxFilter = '';
+  }
 
   toggleImg(): void {
       this.showImg = !this.showImg;
@@ -61,5 +101,13 @@ export class InstructorListComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('In OnInit');
+  }
+
+  performFilter(filterByGearBox: string, filterByCar: string): IInstructor[] {
+    filterByGearBox = filterByGearBox.toLocaleLowerCase();
+    filterByCar = filterByCar.toLocaleLowerCase();
+    return this.instructors
+      .filter((instructor: IInstructor) => instructor.gearbox.toLocaleLowerCase().indexOf(filterByGearBox) !== -1)
+      .filter((instructor: IInstructor) => instructor.car.toLocaleLowerCase().indexOf(filterByCar) !== -1);
   }
 }
