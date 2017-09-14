0c873ac0f78fb2d6862d98b5f76dc2c53874c294
Yuriy Zubaha <yuriy.zubaha@gmail.com1>
2017-09-14 17:27:33 +0300

    Work with instructor-details.component

--- src/app/instructors/instructor-detail.component.ts
+++ src/app/instructors/instructor-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
 import { IInstructor } from "./instructor";
 
 @Component({
@@ -10,9 +12,29 @@ export class InstructorDetailComponent implements OnInit {
   pageTitle = 'Инструктор';
   instructor: IInstructor;
 
-  constructor() { }
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) { }
 
   ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.instructor = {
+      "instructorId": 1,
+      "instructorName": "Владимиров Владимир Алексеевич",
+      "car": "Chevrolet Aveo",
+      "gearbox": "механика",
+      "cityarea": "Центр",
+      "price90min": 190,
+      "starRating": 4.8,
+      "photoUrl": "http://res.cloudinary.com/yuzub/image/upload/v1505128983/panda_vmckn8.jpg",
+      "photoCarUrl": "",
+      "email": "",
+      "phone": ""
+    };
   }
+
+onBack(): void {
+this._router.navigate(['/instructors']);
+}
 
 }
