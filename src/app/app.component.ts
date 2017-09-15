6402f212ca3e02c2ab732b516163b3dec536a1c4
Yuriy Zubaha <yuriy.zubaha@gmail.com1>
2017-09-15 19:23:55 +0300

    Temporary switched to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 import { InstructorService } from "./instructors/instructor.service";
 
 @Component({
@@ -9,4 +11,9 @@ import { InstructorService } from "./instructors/instructor.service";
 })
 export class AppComponent {
   pageTitle = 'Ваш автоинструктор';
+  instructors: FirebaseListObservable<any[]>;
+  constructor(db: AngularFireDatabase) {
+    // db.object('test').subscribe(console.log);
+    this.instructors = db.list('/instructors');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
 import { AppComponent } from './app.component';
 import { InstructorListComponent } from './instructors/instructor-list.component';
 import { InstructorDetailComponent } from './instructors/instructor-detail.component';
 import { WelcomeComponent } from "./home/welcome.component";
 import { InstructorGuardService } from './instructors/instructor-guard.service';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTkUPX3aZjYJe8sGLFX1pTXdMbKC1TN1c",
+  authDomain: "instructor-dp-ua.firebaseapp.com",
+  databaseURL: "https://instructor-dp-ua.firebaseio.com",
+  projectId: "instructor-dp-ua",
+  storageBucket: "instructor-dp-ua.appspot.com",
+  messagingSenderId: "1066323488008"
+};
 
 @NgModule({
   declarations: [
@@ -17,6 +30,9 @@ import { InstructorGuardService } from './instructors/instructor-guard.service';
   imports: [
     BrowserModule,
     FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig), // imports firebase/app needed for everything
+    AngularFireDatabaseModule, // imports firebase/database, only needed for database features
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'instructors', component: InstructorListComponent },
