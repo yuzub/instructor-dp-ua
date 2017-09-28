376dbee36486dc811c29b2018783516fa1a5d788
Yuriy Zubaha <yuriy.zubaha@gmail.com1>
2017-09-28 18:51:40 +0300

    ....

--- src/app/feedbacks/feedback-list.component.ts
+++ src/app/feedbacks/feedback-list.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable } from "angularfire2/database";
 import { IFeedback } from "./feedback";
 import { FeedbackFbService } from "./feedback-fb.service";
+import { InstructorFbService } from '../instructors/instructor-fb.service';
+import { IInstructor } from '../instructors/instructor';
 
 @Component({
   selector: 'ai-feedback-list',
@@ -9,28 +11,34 @@ import { FeedbackFbService } from "./feedback-fb.service";
   styleUrls: ['./feedback-list.component.css']
 })
 export class FeedbackListComponent implements OnInit {
+  instructors$: FirebaseListObservable<IInstructor[]>;
   feedbacks$: FirebaseListObservable<IFeedback[]>;
 
-    pageTitle: string = 'Отзывы';
-    errorMessage: string;
+  pageTitle: string = 'Отзывы';
+  errorMessage: string;
 
-    feedbacks: IFeedback[] = [];
+  feedbacks: IFeedback[] = [];
 
-    constructor(private feedbackFbService: FeedbackFbService) { }
+  constructor(
+    private instructorFbService: InstructorFbService,
+    private feedbackFbService: FeedbackFbService) { }
 
-    ngOnInit(): void {
-      this.getFeedbacks();
+  ngOnInit(): void {
+    this.instructors$ = this.instructorFbService.getInstructors();
+    this.getFeedbacks();
 
-      this.feedbacks$
-        .subscribe(feedbacks => {
-          // console.log(feedbacks);
-          this.feedbacks = feedbacks;
-        },
-        error => this.errorMessage = <any>error);
-    }
+    this.feedbacks$
+      .subscribe(feedbacks => {
+        // console.log(feedbacks);
+        this.feedbacks = feedbacks;
+        console.log(this.feedbacks);
+      },
+      error => this.errorMessage = <any>error);
+  }
 
-    getFeedbacks() {
-      this.feedbacks$ = this.feedbackFbService.getFeedbacks();
-    }
+  getFeedbacks() {
+    this.feedbacks$ = this.feedbackFbService.getFeedbacks();
+    console.log(this.feedbacks$);
+  }
 
 }
